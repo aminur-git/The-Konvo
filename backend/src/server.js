a08efab5f5acc35766fe1731c5e1607b7fb92dca
aminur-git <aminur.4mi@gmail.com>
2025-11-05 20:09:40 +0600

    feat: send message api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import router from "./routes/auth.route.js";
+import messageRouter from "./routes/message.route.js";
 
 dotenv.config()
 
@@ -9,6 +10,7 @@ const port = process.env.PORT;
 
 
 app.use("/api/auth", router)
+app.use("/api/message", messageRouter)
 
 
 app.listen(port, () => {
